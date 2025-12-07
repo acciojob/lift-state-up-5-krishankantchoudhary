5a493dbe79350111cce8c248e98c8bf551896360
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-07 18:02:48 +0530

    rereupload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Log from "./log";
 
-
-
 const App = () => {
-  const[isLoggedIn, setisLoggedIn]=useState(false);
+  const [isLoggedIn, setisLoggedIn] = useState(false);
 
-  const handlelogin=()=>{
+  const handlelogin = () => {
     setisLoggedIn(true);
-  }
+  };
 
   return (
     <div>
-        {isLoggedIn ?
-        (<h2>You are logged in.</h2>):
-        (<Log isLoggedIn={isLoggedIn} onLogin={handlelogin} />)}
+      {isLoggedIn ? (
+        <h2>You are logged in.</h2>
+      ) : (
+        <Log isLoggedIn={isLoggedIn} onLogin={handlelogin} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
